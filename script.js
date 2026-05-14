e9b7d89ac0fd8732b888d588301903ca3e57fb74
Hayato NASU <nasuhayato0832@gmail.com>
2026-05-14 09:29:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function startDiagnosis(){
-
-  alert("動いてる！");
   location.href="question1.html";
-
-
 }
+
+function q1(answer){
+  if(answer ==="a"){
+      action +=2;
+      mental +=1;
+  }
+  else if(answer ==="b"){
+      action +=1;
+  }
+  else if(answer ==="c"){
+      action +=0;
+  }
+  else if(answer ==="d"){
+      action -=1;
+      mental -=1;
+  }
